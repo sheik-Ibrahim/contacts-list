ca7223c3790ff201faaf67af72e2f065a2cad98c
udyoga2002 <udyoga2002@gmail.com>
2020-10-18 17:32:40 +0530

    Trigger rebuild

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Run the app by typing "python app.py" on your terminal.
 print('Hello, World!');
 print('App ready')
-print('This is my app')
+
 
 
 
---
+++ database.py
@@ -0,0 +1,4 @@
+import sqlite3
+import statements
+
+conn = sqlite3.connect('database')
