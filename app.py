c609b93f6b4660b219f1ef904e2d41317b2c9a36
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-10-18 22:38:06 +0530

    Refactor code structure to match PEP8 standards

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-import db
 import statements
+import db as database
 from contacts import Contact
 
 
 def menu():
-    connection = db.connect()
-    db.create_tables(connection)
+    connection = database.connect()
+    database.create_tables(connection)
 
     while (option := input(statements.MENU_PROMPT)) != '4':
         if option == '1':
-            name = input("What is the name of the contact? ")
-            number = input("What is the phone number of the contact? ")
-
-            db.add(connection, name, number)
-
-            print("New contact added to list.")
+            add_new_contact(connection)
         elif option == '2':
-            for name, number in db.get_all(connection):
-                contact = Contact(name, number)
-                print(contact.details())
+            show_all_contacts(connection)
         elif option == '3':
-            name = input("What is the name of the contact? ")
-            (contact_name, contact_number) = db.get_by_name(connection, name)
-            contact = Contact(contact_name, contact_number)
-            print(contact.details())
+            show_requested_contact(connection)
+
+
+def add_new_contact(connection):
+    name = input("What is the name of the contact? ")
+    number = input("What is the phone number of the contact? ")
+    database.add(connection, name, number)
+
+    print("New contact added to list.")
+
+
+def show_all_contacts(connection):
+    for id, name, number in database.get_all(connection):
+        contact = Contact(name, number)
+        print(contact.details())
+
+
+def show_requested_contact(connection):
+    (name, number) = database.get_by_name(
+        connection, input("What is the name of the contact? ")
+    )
+    contact = Contact(name, number)
+    print(contact.details())
 
 
 if __name__ == '__main__':
